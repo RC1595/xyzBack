12fbac5b29a9f27f694c0c87213bd879728b4878
Ryan Carlson <ryan.carlson@mercsystems.ca>
2021-10-23 14:34:45 -0600

    first commit

---
+++ endpoint/user.py
@@ -0,0 +1,4 @@
+from app import app
+from utilities import dbconn
+from flask import request, Response
+import json
---
+++ utilities.py
@@ -0,0 +1,26 @@
+import mariadb
+import dbcreds
+from uuid import uuid4
+
+def dbconn():
+    conn = None
+    cursor = None
+    try:
+        conn = mariadb.connect(
+                                user = dbcreds.user,
+                                password = dbcreds.password,
+                                host = dbcreds.host,
+                                port = dbcreds.port,
+                                database = dbcreds.database)
+        print("connected")
+        cursor = conn.cursor()
+    except:
+        if (cursor != None):
+            cursor.close()
+        print("cursor closed")
+        if (conn != None):
+            conn.close()
+        print("connection closed")
+        raise ConnectionError ("Could not establish a connection to the database")
+    return (conn, cursor)
+
