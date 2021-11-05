848ffd4e0d446c1a55bf558e69badffc5e149350
Ryan Carlson <ryan.carlson@mercsystems.ca>
2021-11-04 21:07:19 -0600

    equipment endpoint less DELETE

--- app/__init__.py
+++ app/__init__.py
@@ -3,4 +3,4 @@ from flask import Flask
 
 app = Flask(__name__)
 
-from endpoint import user, userSession
+from endpoint import user, userSession, equipment
--- endpoint/user.py
+++ endpoint/user.py
@@ -13,30 +13,54 @@ def user():
 
     if request.method == 'GET':
         params = request.args
-        pass
+        userId = params.get('userId')
+        token = request.headers.get('token')
+
+        cursor.execute('SELECT user_id, role FROM user_session INNER JOIN user ON user_session.user_id = user.id WHERE login_token = ?', [token,])
+        result = cursor.fetchone()
+        print(result)
+        
+        if result[1] == 'admin':
+            cursor.execute('SELECT email, role, company_id, created_at, first_name, last_name FROM user')
+            result = cursor.fetchall()
+            userArray = []
+            for user in result:
+                userDict = {
+                    'first_name' : user[4],
+                    'last_name' : user[5],
+                    'email' : user[0],
+                    'role' : user[1],
+                    'company_id' : user[2],
+                    'created_at' : user[3]    
+                }
+                userArray.append(userDict)
+            return Response(json.dumps(userArray, default=str),
+                            mimetype='application/json',
+                            status=200)
+            
+        
+
+
+        else:
+            return Response(json.dumps("error", default=str),
+                        mimetype='application/json',
+                        status=400)
     
     elif request.method == 'POST':
         cursor.execute('SELECT role FROM user INNER JOIN user_session ON user.id = user_session.user_id')
         result = cursor.fetchall()
-        if result == [('super admin',)]:
+        if result[0] == [('admin',)]:
             newUser = request.json.get('first_name')
             pwrd = request.json.get('password')
             salt = bcrypt.gensalt()
             hashed = bcrypt.hashpw(pwrd.encode(), salt)
-            cursor.execute("INSERT INTO user (email, password, role, company, address, phone, first_name, last_name, city, region, country, zip_postal, date_active) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ? )",
+            cursor.execute("INSERT INTO user (email, password, role, first_name, last_name) VALUES (?, ?, ?, ?, ?)",
                             [request.json.get('email'),
                             hashed,
                             request.json.get('role'),
-                            request.json.get('companyName'),
-                            request.json.get('address'),
-                            request.json.get('phoneNumber'),
                             request.json.get('fName'),
                             request.json.get('lName'),
-                            request.json.get('city'),
-                            request.json.get('region'),
-                            request.json.get('country'),
-                            request.json.get('zip'),
-                            request.json.get('activeDate')])
+])
             conn.commit()
             cursor.close()
             conn.close()
