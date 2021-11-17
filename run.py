4f530be7a598ad398ca1eab2767f22a1d40c2198
Ryan Carlson <ryan.carlson@mercsystems.ca>
2021-11-17 16:27:58 -0700

    bjoern

--- run.py
+++ run.py
@@ -1,6 +1,23 @@
 from app import app
-from flask_cors import CORS
+import sys
+host='0.0.0.0'
+port=5000
 
-CORS(app)
+if(len(sys.argv) > 1):
+    mode = sys.argv[1]
+else:
+    print("Invalid mode, please choose either 'production' or 'testing'")
+    exit()
 
-app.run(debug=True)
+if(mode == "production"):
+    import bjoern
+    print("Running in production mode!")
+    bjoern.run(app, "0.0.0.0", 5001)
+elif(mode == "testing"):
+    from flask_cors import CORS
+    CORS(app)
+    print("Running in testing mode!")
+    app.run(debug=True)
+else:
+    print("Invalid mode, please choose either 'production' or 'testing'")
+    exit()
